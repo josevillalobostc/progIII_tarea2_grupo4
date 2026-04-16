a77012b4b462758d60dec3b3b2b3b93200dc08b4
Elías <elias.paz@utec.edu.pe>
2026-04-15 19:28:12 -0500

    Added algorithms variance, transform_reduce and variance_variadic

--- Algoritmos.h
+++ Algoritmos.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Templates.h"
-
+#include <cmath>
 
 
 namespace core_numeric{
@@ -21,6 +21,18 @@ namespace core_numeric{
     }
     
     template<Iterable C>
+    requires Addable<typename C::value_type>
+    double variance(const C& container){
+        using T=typename C::value_type;
+        T result{};
+        size_t _=0;
+        for(const auto& value:container){
+            result=result+pow(value-core_numeric::mean(value:container),2);_++;
+        }
+        result=result/container.size()-1;
+    }
+
+    template<Iterable C>
     requires Comparable<typename C::value_type>
     auto max(const C& container){
         using T = typename C::value_type;
@@ -35,6 +47,15 @@ namespace core_numeric{
         return max;
     }
     
+    template<Iterable C,typename Func>
+    requires Addable<invoke_result_t<Func, typename C::value_type>>
+    auto transform_reduce(const C& container, Func transformer) {
+        using ResultType = std::invoke_result_t<Func, typename C::value_type>;
+        ResultType result{};
+        for (const auto& item:container){result=result+transformer(item);}
+        return result;
+    }
+
     template<typename... Ts>
     requires (Addable<Ts> && ...)
     auto sum_variadic(Ts... xs){
@@ -47,7 +68,15 @@ namespace core_numeric{
         auto suma = (xs + ...);
         return suma / sizeof...(xs);
     }
-    
+
+    double variance_variadic(Ts...xs){
+    double mean=mean_variadic(xs...);
+    double suma=(pow(static_cast<double>(xs)-m,2)+...);
+    size_t n=sizeof...(xs);
+    if(n<=1){return 0.0};
+    return sum_sq_diff/(n - 1);
+    }
+
     template<typename T, typename... Ts>
     requires (Comparable<T> && ... && Comparable<Ts>)
     auto max_variadic(T prim, Ts... rest){
